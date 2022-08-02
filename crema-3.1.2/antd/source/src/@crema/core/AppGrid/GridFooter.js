5875f259521f60f9ab0492320b057c3e510dcd00
nandeep21 <nandeep@openxcellinc.com>
2022-08-02 18:10:11 +0530

    theme commit

---
+++ crema-3.1.2/antd/source/src/@crema/core/AppGrid/GridFooter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Progress} from 'antd';
+import PropTypes from 'prop-types';
+
+import './index.style.less';
+const GridFooter = ({loading, footerText}) => {
+  if (loading) {
+    return (
+      <div className='loader-progress'>
+        <Progress percent={30} />
+        <span>Loading...</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className='list-footer'>
+        <p>{footerText}</p>
+      </div>
+    );
+  }
+};
+
+GridFooter.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  footerText: PropTypes.string.isRequired,
+};
+
+GridFooter.defaultProps = {
+  loading: false,
+  footerText: '',
+};
+export default GridFooter;
